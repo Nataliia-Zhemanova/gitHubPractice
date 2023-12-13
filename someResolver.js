10cc6ffb8f2d037941cf4d420174d417b22bed3d
Sergei Swift <sergei.swift@icloud.com>
2023-12-12 18:30:11 -0800

    palindrom

---
+++ someResolver.js
@@ -0,0 +1,10 @@
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/\s/g, '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
+}
+
+const exampleString = "А роза упала на лапу Азора";
+const result = isPalindrome(exampleString);
+
+console.log(result);
